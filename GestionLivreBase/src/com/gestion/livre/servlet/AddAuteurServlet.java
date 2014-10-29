33a336e1833ab72a05059b065d07aa2c11ca6008
Maxime63 <maxime.chauvet@bbox.fr>
2014-10-29 19:15:31 +0100

    create first servlet

---
+++ GestionLivreBase/src/com/gestion/livre/servlet/AddAuteurServlet.java
@@ -0,0 +1,23 @@
+package com.gestion.livre.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class AddAuteurServlet extends HttpServlet {
+
+	/***/
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello World !");
+		resp.getWriter().println("It works !");
+	}
+}
